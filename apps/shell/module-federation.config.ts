246b2a53737bd8464027e3f7dfa80aaf2eedbfc0
MrCoding2000 <taha.softwareengineer2000@gmail.com>
2024-11-19 12:33:58 -0800

    add libs directory

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['user_management'],
+  remotes: ['user_management', 'reports', 'payments', 'security'],
 };
 
 /**
--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -3,6 +3,18 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'security',
+    loadChildren: () => import('security/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
+    path: 'payments',
+    loadChildren: () => import('payments/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
+    path: 'reports',
+    loadChildren: () => import('reports/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'user_management',
     loadChildren: () =>
       import('user_management/Routes').then((m) => m!.remoteRoutes),
